143e0d4f4cdf988136f720c02a27f54473998b23
artuone83 <artur.wozniak@codetain.co>
2020-06-02 16:26:06 +0200

    remove Component import

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState, useRef } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { render } from "react-dom";
 import Hello from "./components/Hello";
 import Navigation from "./components/Navigation";
